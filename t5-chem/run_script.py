b5977fd5fe12878c3471e727cfb06af495aa9469
Stanley Lo <stanley.lo@mail.utoronto.ca>
2024-05-08 12:14:24 -0400

    developing training loop

--- t5-chem/run_script.py
+++ t5-chem/run_script.py
@@ -2,9 +2,10 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from pathlib import Path
 import pandas as pd
-import sklearn
+import sklearn.model_selection
+import torch
 
-# tokenizer = AutoTokenizer.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
+tokenizer = AutoTokenizer.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
 # model = AutoModelForSeq2SeqLM.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
 
 # import data (local import, change path to your data)
@@ -15,5 +16,19 @@ data = pd.read_csv(data_path)
 X_train, X_test = sklearn.model_selection.train_test_split(data["SMILES"], test_size=0.2, random_state=42)
 Y_train, Y_test = sklearn.model_selection.train_test_split(data["zpve"], test_size=0.2, random_state=42)
 
+# convert pandas dataframe to list for tokenization
+X_train = X_train.tolist()
+X_test = X_test.tolist()
+
+# convert pandas datafram to tensor
+Y_train = torch.tensor(Y_train.tolist())
+Y_test = torch.tensor(Y_test.tolist())
+
 # tokenize data
-# testing
+X_train = tokenizer(X_train, return_tensors="pt", padding=True, truncation=True)
+X_test = tokenizer(X_test, return_tensors="pt", padding=True, truncation=True)
+print(X_train, X_test)
+
+# create dataloader
+train_dataset = torch.utils.data.TensorDataset(X_train, Y_train, batch_size=8, shuffle=True)
+test_dataset = torch.utils.data.TensorDataset(X_test, Y_test, batch_size=8, shuffle=False)
