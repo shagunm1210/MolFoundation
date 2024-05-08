0faeeeeb00d6f32e139c47ac764fc5d503961bce
Stanley Lo <stanley.lo@mail.utoronto.ca>
2024-05-08 11:59:27 -0400

    first commit t5-chem

---
+++ t5-chem/run_script.py
@@ -0,0 +1,18 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from pathlib import Path
+import pandas as pd
+import sklearn
+
+# tokenizer = AutoTokenizer.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
+# model = AutoModelForSeq2SeqLM.from_pretrained("GT4SD/multitask-text-and-chemistry-t5-base-augm")
+
+# import data (local import, change path to your data)
+data_path: Path = Path("../../../Datasets/qm9/gdb9_wSmiles.csv")
+data = pd.read_csv(data_path)
+
+# split data
+X_train, X_test = sklearn.model_selection.train_test_split(data["SMILES"], test_size=0.2, random_state=42)
+Y_train, Y_test = sklearn.model_selection.train_test_split(data["zpve"], test_size=0.2, random_state=42)
+
+# tokenize data
